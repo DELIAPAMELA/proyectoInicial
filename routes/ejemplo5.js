a88eded77498b431556b83454a7fbb2464f212ea
DELIAPAMELA <pame.cielo123@gmail.com>
2015-10-31 09:51:28 -0400

    ejemplos operadores

---
+++ routes/ejemplo5.js
@@ -0,0 +1,6 @@
+console.log("suma de numeros 13 + 7 : ");
+console.log(13 + 7);
+console.log("signo de un numero +13: ");
+console.log( +13);
+console.log("concatenacion de strings \"hola \" + \"pepe\" : " );
+console.log("hola " + "pepe");
---
+++ routes/ejemplo7.js
@@ -0,0 +1,7 @@
+console.log("typeof 7=>" + typeof 7);
+console.log("typeof \"hola\=>"+ typeof "hola");
+console.log("typeof true=>"+ typeof "hola");
+console.log("typeof undefined=>"+ typeof undefined)
+console.log("typeof null=>" + typeof null)
+console.log("typeof new date()=>"+ typeof new date())
+console.log("typeof new function()=>"+ typeof new function())
